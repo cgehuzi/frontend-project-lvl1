8b7d6f60d851d5de94c656a31bc70cc2f37e96ba
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2021-12-28 20:47:18 +0300

    fix: change files for eslint rules

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readLineSync from "readline-sync";
+import readLineSync from 'readline-sync';
 
-export const getName = () => {
-  const name = readLineSync.question("May I have your name? ");
+export default () => {
+  const name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
