ccdd7e1c7886c8b5f227ee2e10300e0c176b30a7
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2021-12-29 10:49:19 +0300

    feat(5): add brain-even game

---
+++ bin/brain-even.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+import getPlayerName, { getAnswer } from '../src/cli.js';
+
+const playerName = getPlayerName();
+const roundCount = 3;
+
+const evenRound = () => {
+  const question = Math.round(Math.random() * 100);
+  const coorrectAnswer = question % 2 === 0 ? 'yes' : 'no';
+
+  const playerAnswer = getAnswer(question);
+
+  return [coorrectAnswer, playerAnswer];
+};
+
+const evenGame = (rounds) => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 1; i <= rounds; i += 1) {
+    const [coorrectAnswer, playerAnswer] = evenRound();
+
+    if (coorrectAnswer === playerAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(
+        `'${playerAnswer}' is wrong answer ;(. Correct answer was '${coorrectAnswer}'.`,
+      );
+      console.log(`Let's try again, ${playerName}!`);
+      return false;
+    }
+  }
+
+  console.log(`Congratulations, ${playerName}!`);
+  return true;
+};
+
+evenGame(roundCount);
