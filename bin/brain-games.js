4dd8789f41a80007d623efe9c4fa258234737abf
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2021-12-30 00:35:59 +0300

    feat: add brain-games choosing

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,45 @@
 #!/usr/bin/env node
 
-import getPlayerName from '../src/cli.js';
+import getPlayerName, { getGameSelection } from '../src/cli.js';
+import startGame from '../src/index.js';
 
-getPlayerName();
+import evenRound, { gameRules as evenRules } from '../games/brain-even.js';
+import calcRound, { gameRules as calcRules } from '../games/brain-calc.js';
+import gcdRound, { gameRules as gcdRules } from '../games/brain-gcd.js';
+import progressionRound, { gameRules as progressionRules } from '../games/brain-pregression.js';
+import primeRound, { gameRules as primeRules } from '../games/brain-prime.js';
+
+const playerName = getPlayerName();
+const selectedGame = getGameSelection();
+
+switch (selectedGame) {
+  case '1':
+  case 'brain-even':
+    startGame(evenRound, evenRules, playerName);
+    break;
+
+  case '2':
+  case 'brain-calc':
+    startGame(calcRound, calcRules, playerName);
+    break;
+
+  case '3':
+  case 'brain-gcd':
+    startGame(gcdRound, gcdRules, playerName);
+    break;
+
+  case '4':
+  case 'brain-progression':
+    startGame(progressionRound, progressionRules, playerName);
+    break;
+
+  case '5':
+  case 'brain-prime':
+    startGame(primeRound, primeRules, playerName);
+    break;
+
+  default:
+    console.log(`Sorry, but game '${selectedGame}' was not found ;(.`);
+    console.log(`Let's try again, ${playerName}!`);
+    break;
+}
--- src/cli.js
+++ src/cli.js
@@ -13,5 +13,18 @@ const getAnswer = (question) => {
   return answer;
 };
 
+const getGameSelection = () => {
+  console.log('You can play one of the games:');
+  console.log('1 : brain-even');
+  console.log('2 : brain-calc');
+  console.log('3 : brain-gcd');
+  console.log('4 : brain-progression');
+  console.log('5 : brain-prime');
+  console.log('Which game do you choose?');
+
+  const answer = readLineSync.question('Number or name: ');
+  return answer;
+};
+
 export default getPlayerName;
-export { getAnswer };
+export { getAnswer, getGameSelection };
