b8bd0bc2f3c1d12881906555ac2fd161aad9aca2
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2021-12-29 16:51:31 +0300

    fix(even): fix function name

--- games/brain-even.js
+++ games/brain-even.js
@@ -2,12 +2,12 @@ import _ from 'lodash';
 
 const evenRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const gameRound = () => {
+const evenRound = () => {
   const question = _.random(0, 100);
   const coorrectAnswer = question % 2 === 0 ? 'yes' : 'no';
 
   return [question, coorrectAnswer];
 };
 
-export default gameRound;
+export default evenRound;
 export { evenRules };
