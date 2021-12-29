cf5d28078c56c5328cc26118c2d7e9777175183f
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2021-12-29 09:43:05 +0300

    fix: refactoring cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import getName from '../src/cli.js';
+import getPlayerName from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-getName();
+getPlayerName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readLineSync from 'readline-sync';
 
 export default () => {
+  console.log('Welcome to the Brain Games!');
   const name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
 };
