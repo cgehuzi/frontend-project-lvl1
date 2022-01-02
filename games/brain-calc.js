c19b5a3a4c95937b25aea203fffd1d5380f84e92
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2022-01-02 14:13:49 +0300

    fix(calc): relocate getExpressionResult to abstraction

--- games/brain-calc.js
+++ games/brain-calc.js
@@ -3,22 +3,25 @@ import _ from 'lodash';
 const gameRules = 'What is the result of the expression?';
 const expressions = ['+', '-', '*'];
 
+const getExpressionResult = (expression, number1, number2) => {
+  switch (expression) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
+    default:
+      return null;
+  }
+};
+
 const gameRound = () => {
   const [number1, number2] = [_.random(0, 25), _.random(0, 25)];
   const expression = expressions[_.random(0, expressions.length - 1)];
 
   const question = `${number1} ${expression} ${number2}`;
-  let coorrectAnswer = null;
-
-  if (expression === '+') {
-    coorrectAnswer = number1 + number2;
-  }
-  if (expression === '-') {
-    coorrectAnswer = number1 - number2;
-  }
-  if (expression === '*') {
-    coorrectAnswer = number1 * number2;
-  }
+  const coorrectAnswer = getExpressionResult(expression, number1, number2);
 
   return [question, String(coorrectAnswer)];
 };
