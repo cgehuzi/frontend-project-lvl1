45829eb5963749738df1f9d4db78734cd1248216
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2022-01-02 14:12:34 +0300

    fix(even): relocate isEven to abstraction

--- games/brain-even.js
+++ games/brain-even.js
@@ -2,9 +2,11 @@ import _ from 'lodash';
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (number) => number % 2 === 0;
+
 const gameRound = () => {
   const question = _.random(0, 100);
-  const coorrectAnswer = question % 2 === 0 ? 'yes' : 'no';
+  const coorrectAnswer = isEven(question) ? 'yes' : 'no';
 
   return [question, coorrectAnswer];
 };
